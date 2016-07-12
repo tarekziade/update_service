a79505e6f35f8be8a9afce1e140fbc0c73c3175a
Tarek Ziade <tarek@ziade.org>
2016-07-12 16:19:22 +0200

    cosmetics

--- src/aws.rs
+++ src/aws.rs
@@ -7,7 +7,8 @@ use self::rusoto::s3::S3Helper;
 
 pub fn write_s3_file(bucket_name: &str, filename: &str, content: &str) {
     let _ = env_logger::init();
-    let s3 = S3Helper::new(DefaultCredentialsProvider::new().unwrap(), Region::UsWest2);
+    let provider = DefaultCredentialsProvider::new().unwrap();
+    let s3 = S3Helper::new(provider, Region::UsWest2);
     let response = s3.put_object(bucket_name, filename, content.as_bytes()).unwrap();
 
     println!("Written in S3: {:?}", response);
